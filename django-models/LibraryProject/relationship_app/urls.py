753081f94297614e61b0599ca345c980a8b4090f
richeasy001 <tenebe.osikhena@gmail.com>
2024-11-30 13:51:17 +0100

    login logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, CustomLoginView, CustomLogoutView, register
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
 ]
